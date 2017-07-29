e406fee79f66ea5f658ed5fa239d0de8a117851d
Khoi Phan <khoiphan21@gmail.com>
2017-07-29 17:05:09 +1000

    finished demographic data for nundah

--- src/app/classes/demographic.ts
+++ src/app/classes/demographic.ts
@@ -2,4 +2,6 @@ export class Demographic {
     totalPopulation: number;
     female: number;
     male: number;
+    averageDisposableIncome: number; // weekly
+    familyHouseholds: number; // As a percentage
 }
--- src/app/data/nundah.ts
+++ src/app/data/nundah.ts
@@ -4,6 +4,8 @@ export const NUNDAH: Suburb = {
     demographic: {
         totalPopulation: 12141,
         male: 6006,
-        female: 6133
+        female: 6133,
+        averageDisposableIncome: 334, // Weekly
+        familyHouseholds: 2956
     }
 }
