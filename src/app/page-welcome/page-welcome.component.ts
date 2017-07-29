ca304a2158fb67d95b28164476ffe9672c35752e
Khoi Phan <khoiphan21@gmail.com>
2017-07-29 14:52:46 +1000

    added option for main page

--- src/app/page-welcome/page-welcome.component.ts
+++ src/app/page-welcome/page-welcome.component.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./page-welcome.component.scss']
 })
 export class PageWelcomeComponent implements OnInit {
+  isSuggestionShown: boolean = false;
+  inputText: string = '';
+  address: string = '1/1207 Sandgate Rd, Nundah QLD 4102';
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  showSuggestion(event: Event) {
+    this.isSuggestionShown = true;
+    event.preventDefault();
+    event.stopPropagation();
+  }
+  hideSuggestion() {
+    this.isSuggestionShown = false;
+  }
+  selectAddress() {
+    this.inputText = this.address;
   }
 
 }
