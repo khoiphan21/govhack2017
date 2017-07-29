88e3db5254805ed3bae544b9c750ef6df4fe89e1
Jeremy Yung Hau Ng <jeremy.ng1002@gmail.com>
2017-07-29 14:16:32 +1000

    d3 implementation beginning

--- src/app/page-data/page-data.component.ts
+++ src/app/page-data/page-data.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
+// import the D3 service, the type alias for the d3 varialbe and selection interface
+import { D3Service, D3, Selection } from 'd3-ng2-service'; 
 
 @Component({
   selector: 'app-page-data',
   templateUrl: './page-data.component.html',
-  styleUrls: ['./page-data.component.css']
+  styleUrls: ['./page-data.component.scss']
 })
 export class PageDataComponent implements OnInit {
+  private d3: D3;
+  private parentNativeElement: any;
 
-  constructor() { }
+  constructor(element: ElementRef, d3Service: D3Service) { // passing d3 service into the consturctor
+    this.d3 = d3Service.getD3(); // <-- obtain the d3 object from the D3 service
+    this.parentNativeElement = element.nativeElement;
+   }
 
   ngOnInit() {
+    let d3 = this.d3; // for convenience use a block scope variable
+    let d3ParentElement: Selection<any,any,any,any>; // Use  the selection interface
+
+    if (this.parentNativeElement !== null){
+      d3ParentElement = d3.select(''); // use the d3 select method
+    }
   }
 
 }
