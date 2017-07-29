41c89205b21950fe117082b49efb0bd6d06c8adf
Jeremy Yung Hau Ng <jeremy.ng1002@gmail.com>
2017-07-29 23:34:55 +1000

    new component

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -5,13 +5,15 @@ import { CommonModule } from '@angular/common';
 import { PageWelcomeComponent } from '../page-welcome/page-welcome.component';
 import { PageStatsComponent } from '../page-stats/page-stats.component';
 import { PageMapComponent } from '../page-map/page-map.component';
+import { PageStatsP2Component } from '../page-stats-p2/page-stats-p2.component';
 
 const routes: Routes = [
  { path: '', redirectTo: '/welcome', pathMatch: 'full' },
  { path: 'welcome', component: PageWelcomeComponent,
   children: [
     { path: 'stats', component: PageStatsComponent },
-    { path: 'maps', component: PageMapComponent }
+    { path: 'maps', component: PageMapComponent },
+    { path: 'stats2', component: PageStatsP2Component }
   ] 
   },
 ]
